8d048ac7af476a48bd6e3be68e939c11356b7b0a
Missmacharia <yvonnemacharia30@gmail.com>
2023-09-07 21:30:37 +0300

    worked on the endpoints

--- src/auth-api/controllers/authController.ts
+++ src/auth-api/controllers/authController.ts
@@ -1,44 +1,54 @@
 import { Response, Request } from "express";
 
-// import { Pool } from "pg";
+import { pool } from "../config";
 
-import { pool } from "../config/index";
+// import { Pool } from "pg";
 
 import { v4 } from "uuid";
 
 const bcrypt = require("bcrypt");
 
-export const registeration = async (res: Response, req: Request) => {
+const registerController = async (res: Response, req: Request) => {
   try {
     // request from the body
+
     const { username, email, age, gender, userdetails } = req.body();
 
-    //   generating random ids
+    // generating random ids
 
     const userid = v4();
 
-    //generating a salt
+    // generating a salt
 
     const salt = await bcrypt.genSalt(10);
     const password = await bcrypt.hash(req.body.password, salt);
 
-    // connection to the database
-    const newUser = pool.query(
-      `
-      insert into users (userid, username, email, gender, age ,password, userid)
-    values (userid, username, email, gender, age, password, userid)`,
+    // check if the email or username already exists
+    // connect to the db
+    const existUser = await pool.query(
+      `select * from user where userid  = userid or email = email`,
+      [username, email]
+    );
+
+    if (existUser.rows.length > 0) {
+      return res
+        .status(400)
+        .json({ message: "user already exist. Please sign up again" });
+    }
+
+    // insert into the database
+
+    const signup = await pool.query(
+      `insert into users (userid, username, email, age, gender, userdetails)`,
       [userid, username, email, age, gender, password, userdetails]
     );
 
-    console.log(newUser);
-
-    // res.status(200).json((await newUser).rows[0]);
-    res.status(200).json({
-      message: "Sign up was successful",
-    });
+    return res
+      .status(200)
+      .json({ message: "Registeration successful", signup });
   } catch (error) {
-    res.status(400).json({
-      message: "Sign up failed",
-    });
+    return res.status(400).json({ message: "Registration failed" });
   }
 };
+
+export default registerController;
